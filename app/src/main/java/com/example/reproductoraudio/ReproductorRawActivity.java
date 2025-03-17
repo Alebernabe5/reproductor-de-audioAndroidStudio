7c08021c84533cf5571f97a8399edc6531d5d7cc
Alejandro <alebernadev@gmail.com>
2025-03-17 14:25:04 +0100

    Establezco el play

--- app/src/main/java/com/example/reproductoraudio/ReproductorRawActivity.java
+++ app/src/main/java/com/example/reproductoraudio/ReproductorRawActivity.java
@@ -1,6 +1,10 @@
 package com.example.reproductoraudio;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ReproductorRawActivity extends AppCompatActivity {
+
+    protected TextView texto1;
+    protected ImageButton botonPause, botonPlay, botonStop;
+    protected MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,37 @@ public class ReproductorRawActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        texto1 = (TextView) findViewById(R.id.texto1_raw);
+        botonPause = (ImageButton) findViewById(R.id.imabotonPause_raw);
+        botonPlay = (ImageButton) findViewById(R.id.imabotonPlay_raw);
+        botonStop = (ImageButton) findViewById(R.id.imabotonStop_raw);
+
+        //BOTON PLAY
+        botonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mp = MediaPlayer.create(ReproductorRawActivity.this, R.raw.cancion1);
+                mp.start();;
+            }
+        });
+
+        //BOTON PAUSE
+        botonPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        //BOTON STOP
+        botonStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
